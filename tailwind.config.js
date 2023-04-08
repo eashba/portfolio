82a537333a3eba794ff4bc5f1244760ce855c920
Evan Ashba <eashba@Evans-MacBook-Pro-2.local>
2023-04-08 16:41:26 -0400

    tailwindconfig, gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,6 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ], theme: {
-        fontFamily: {
-            'fancy': ['Playfair Display'],
-        },
         container: {
             center: true,
         },
